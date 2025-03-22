5e82585a06670905cf036b50ffd6de582e84bf3d
suntfff <voovo.vtta@mail.ru>
2025-03-22 18:07:18 +0200

    Create 2.php

---
+++ lab3/2.php
@@ -0,0 +1,18 @@
+<?php
+
+$number1 = 6;
+echo "$number1";
+
+echo "\n";
+$number2 = 3.5;
+echo $number1*2;
+
+echo "\n";
+$lastMonth = 475;
+$thisMonth = 567;
+$difference = abs($lastMonth-$thisMonth);
+echo $difference;
+
+?>
+
+
