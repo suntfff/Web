0b6a2261f1bcb9a74dbdf384235b1c5bd03f3b3e
suntfff <voovo.vtta@mail.ru>
2025-04-05 15:52:12 +0200

    Update Session.php

--- lab4/Task4/Session.php
+++ lab4/Task4/Session.php
@@ -1,12 +1,11 @@
 <?php
 session_start();
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $_SESSION['phone_brand'] = $_POST['phone_brand'];
-    $_SESSION['phone_model'] = $_POST['phone_model'];
-    $_SESSION['price'] = $_POST['price'];
-    echo "Phone brand: " . $_SESSION['phone_brand'] . "<br>";
-    echo "Phone model: " . $_SESSION['phone_model'] . "<br>";
-    echo "Price: " . $_SESSION['price'] . "<br>";
-}
+$_SESSION['phone_brand'] = $_POST['phone_brand'];
+$_SESSION['phone_model'] = $_POST['phone_model'];
+$_SESSION['price'] = $_POST['price'];
+echo "Phone brand: " . $_SESSION['phone_brand'] . "<br>";
+echo "Phone model: " . $_SESSION['phone_model'] . "<br>";
+echo "Price: " . $_SESSION['price'] . "<br>";
+
 ?>
