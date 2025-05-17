2b259c49ee50f7892c9b67a8e6fcf07d89dd51c9
suntfff <voovo.vtta@mail.ru>
2025-05-17 14:05:43 +0200

    Update save.php

--- lab7/code/save.php
+++ lab7/code/save.php
@@ -1,21 +1,26 @@
 <?php
-if (!isset($_POST["email"], $_POST["category"], $_POST["title"], $_POST["description"])) {
+if (!isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description'])) {
     header('Location: /');
-    exit();
+    exit;
 }
-$email = $_POST["email"];
-$category = $_POST["category"];
-$title = $_POST["title"];
-$description = $_POST["description"];
-$categoryDir = "categories/$category";
-if (!is_dir($categoryDir)) {
-    mkdir($categoryDir, 0777, true);
+$email       = $_POST['email'];
+$category    = $_POST['category'];
+$title       = $_POST['title'];
+$description = $_POST['description'];
+$mysqli = new mysqli('db', 'root', 'helloworld', 'web');
+if (mysqli_connect_errno()) {
+    printf("Подключение к серверу MySQL невозможно. Код ошибки: %s\n", mysqli_connect_error());
+    exit;
 }
-$file_path = "$categoryDir/{$title}.txt";
-$content = $email . "\n" . $description;
-if (false === file_put_contents($file_path, $content)) {
-    throw new Exception("Something went wrong");
+$stmt = $mysqli->prepare(
+    'INSERT INTO ad (email, category, title, description) VALUES (?, ?, ?, ?)'
+);
+$stmt->bind_param('ssss', $email, $category, $title, $description);
+
+if (!$stmt->execute()) {
+    throw new Exception('Ошибка вставки в БД: ' . $stmt->error);
 }
-header("Location: /");
-exit();
-?>
+$stmt->close();
+$mysqli->close();
+header('Location: /');
+exit;
